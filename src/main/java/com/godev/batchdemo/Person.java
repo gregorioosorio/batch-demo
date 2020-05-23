af27cfc260fdbc97054ff5fafb8a62f8eb848e69
Gregorio Osorio <gosorio@Gregorios-MacBook-Air.local>
2020-05-23 11:54:15 -0500

    first commit

---
+++ src/main/java/com/godev/batchdemo/Person.java
@@ -0,0 +1,17 @@
+package com.godev.batchdemo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Person {
+	private String firstName;
+	private String lastName;
+}
