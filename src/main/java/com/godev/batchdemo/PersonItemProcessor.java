13c79da2d54bd65c3986b45818d99f89387845ef
Gregorio Osorio <gosorio@Gregorios-MacBook-Air.local>
2020-05-29 23:25:54 -0500

    add info that come from header

--- src/main/java/com/godev/batchdemo/PersonItemProcessor.java
+++ src/main/java/com/godev/batchdemo/PersonItemProcessor.java
@@ -1,24 +1,29 @@
 package com.godev.batchdemo;
 
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.item.ItemProcessor;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class PersonItemProcessor implements ItemProcessor<Object, Person> {
+	
+	private StepExecution stepExecution;
 
 	@Override
 	public Person process(final Object object) throws Exception {
 		Person person = new Person();
 		if(object instanceof String) {
-			person.setTitle(object.toString());
-			person.setFirstName("Default");
-			person.setLastName("Default");
+			stepExecution.getExecutionContext().putString("title", object.toString());
+			return null;
 		} else if(object instanceof Person) {
 			Person p = (Person)object;
-			person.setTitle("Default");
+			person.setTitle(stepExecution.getExecutionContext().getString("title"));
 			person.setFirstName(p.getFirstName());
 			person.setLastName(p.getLastName());
+		} else {
+			return null;
 		}
 		final String title = person.getTitle().toUpperCase();
 		final String firstName = person.getFirstName().toUpperCase();
@@ -31,4 +36,9 @@ public class PersonItemProcessor implements ItemProcessor<Object, Person> {
 		return transformedPerson;
 	}
 
+	
+    @BeforeStep
+    public void saveStepExecution(StepExecution stepExecution) {
+        this.stepExecution = stepExecution;
+    }
 }
